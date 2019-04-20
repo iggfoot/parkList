33cd35851b03b7aa4ac131f2f13b6468dfc97b33
iggfoot <ijneville19@gmail.com>
2019-04-20 12:42:30 -0400

    'added api key'

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("hello sucker")
+/*Requirements:
+The user must be able to search for parks in one or more states.
+The user must be able to set the max number of results, with a default of 10.
+The search must trigger a call to NPS's API.
+The parks in the given state must be displayed on the page. Include at least:
+Full name
+Description
+Website URL
+The user must be able to make multiple searches and see only the results for the current search.
+As a stretch goal, try adding the park's address to the results.*/
+
+
+
+
+
+
+
+const apiKey = "B84sI5wVOXQwODDsa5G0KNgJCajRBXCapV0mJhw3";
+const searchURL = `https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=${apiKey}`;
+
+function getParkInfo() {
+    const options = {
+        headers: new Headers({
+            "X-Api-Key": apiKey})
+        };
+        
+}
+
+
